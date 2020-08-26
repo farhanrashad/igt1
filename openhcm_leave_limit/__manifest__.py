4883ba12a3ef6c23881ab3e73bd0b45d8236da3f
dynexcel-ddc1 <ddc1@dynexcel.co>
2020-08-26 20:27:20 +0500

    updated

--- openhcm_leave_limit/__manifest__.py
+++ openhcm_leave_limit/__manifest__.py
@@ -20,7 +20,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base', 'hr_holidays'],
+    'depends': ['base', 'hr_holidays','hr'],
 
     # always loaded
     'data': [
