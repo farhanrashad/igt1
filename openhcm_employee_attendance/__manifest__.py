e45995bd01abd7f3da35884cb9d44cd004e63037
dynexcel-ddc1 <ddc1@dynexcel.co>
2020-08-26 20:52:22 +0500

    update

--- openhcm_employee_attendance/__manifest__.py
+++ openhcm_employee_attendance/__manifest__.py
@@ -20,7 +20,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['hr','report_xlsx'],
+    'depends': ['hr','report_xlsx','hr_attendance'],
 
     # always loaded
     'data': [
