f16b0b0c6e2692bb3e503aef8bce4b265b9ae176
dynexcel-ddc1 <ddc1@dynexcel.co>
2020-09-05 17:37:22 +0500

    visitor pass

--- de_visitor_pass/models/visitor_visit_details.py
+++ de_visitor_pass/models/visitor_visit_details.py
@@ -1,10 +1,18 @@
 # -*- coding: utf-8 -*-
 
 from odoo import api, fields, models, _
+import datetime
 
 
 class VisitDetail(models.Model):
     _name = "visit.detail"
+
+#   Making necessary states
+    state = fields.Selection([
+        ('draft', 'Draft'),
+        ('in', 'In'),
+        ('out', 'Out')
+    ], readonly=True, index=True, copy=False)
 
 #   declaring fieldss for 1 group visitorr details
     det_name= fields.Char(string='Name',required=True)
@@ -18,23 +26,55 @@ class VisitDetail(models.Model):
     cat=fields.Many2one("visit.category",string="Visitor Category")
 
 #   declaring group for  checkin check out
-    check_in=fields.Datetime(string="Check In",required=True)
-    check_out=fields.Datetime(string="Check Out")
+    check_in=fields.Datetime(string="Check In",readonly=True)
+    check_out=fields.Datetime(string="Check Out",readonly=True)
 
 #     declairing group for Contact Details
     reference_id=fields.Many2one("hr.employee",string="Reference")
-    department_id=fields.Many2one("hr.department",string="Department")
-    employee_id=fields.Many2one("hr.employee",string="Employee")
+    department_id=fields.Many2one("hr.employee",string="Department")
+    employee_id=fields.Many2one("hr.department",string="Employee")
 
 #   declaring comment for notebook
-    comment=fields.Text(string="Add Comment")
-
+    comment=fields.Html()
 #     sequence generation
     name=fields.Char(string="Visitor Sequence",required=True,copy=False,readonly=True,index=True,default=lambda self: _('New'))
 
+    # @api.model
+    # def write(self, vals):
+    #     print("write----")
+    #     vals['state'] = 'draft'
+    #     return super(VisitDetail, self).write(vals)
+
+    # adding new field total duration
+    total_duration = fields.Float(string="Total Duration", default="_get_total_duration")
+
+    def _get_total_duration(self):
+        print("printing difference ",self.check_out-self.check_in)
+        f =self.check_out
+        s = self.check_in
+        q = f - s
+        d_s = q.total_seconds()
+        print("in seconds----", d_s)
+        hours = divmod(d_s, 3600)[0]
+        print(type(hours))
+        self.total_duration=hours
+
+
     @api.model
     def create(self,vals):
+        vals['state'] = 'draft'
+        print("called",vals['state'])
         if vals.get('name', _('New')) == _('New'):
             vals['name'] = self.env['ir.sequence'].next_by_code('visitor.sequence') or _('New')
         result = super(VisitDetail, self).create(vals)
         return result
+
+    # defining actions for states
+    def set_checkIn(self):
+        self.state='in'
+        self.check_in=datetime.datetime.today()
+
+
+    def set_checkOut(self):
+        self.state = 'out'
+        self.check_out=datetime.datetime.today()
