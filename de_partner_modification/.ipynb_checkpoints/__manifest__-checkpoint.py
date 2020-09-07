0b118325c2a18bd32db3b529377c2e4b655271ed
dynexcel-ddc1 <ddc1@dynexcel.co>
2020-09-07 12:09:21 +0500

    partner updated

--- de_partner_modification/.ipynb_checkpoints/__manifest__-checkpoint.py
+++ de_partner_modification/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -20,11 +20,12 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','sale','purchase','account','stock'],
+    'depends': ['base','sale','purchase','account','stock','mail'],
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/ir.model.access.csv',
+        'security/security.xml',
         'views/views.xml',
         'views/templates.xml',
     ],
--- de_partner_modification/models/.ipynb_checkpoints/models-checkpoint.py
+++ de_partner_modification/models/.ipynb_checkpoints/models-checkpoint.py
@@ -4,7 +4,7 @@ from odoo import models, fields, api, _
 
 
 class PartnerModification(models.Model):
-    _name = 'res.partner'
+    _inherit = 'res.partner'
     
     def _get_default_stage_id(self):
         return self.env['partner.stages'].search([], limit=1).id
@@ -26,29 +26,11 @@ class PartnerStages(models.Model):
     
     
 
+    def _get_default_project_ids(self):
+        default_project_id = self.env.context.get('default_project_id')
+        return [default_project_id] if default_project_id else None
+
     name = fields.Char(string='Stage Name', required=True, translate=True)
-    description = fields.Text(translate=True)
-    sequence = fields.Integer(default=1)
-    active = fields.Boolean(default=True)
-    unattended = fields.Boolean(
-        string='Unattended')
-    closed = fields.Boolean(
-        string='Closed')
-    mail_template_id = fields.Many2one(
-        'mail.template',
-        string='Email Template',
-        domain=[('model', '=', 'helpdesk.ticket')],
-        help="If set an email will be sent to the "
-             "customer when the ticket"
-             "reaches this step.")
-    fold = fields.Boolean(
-        string='Folded in Kanban',
-        help="This stage is folded in the kanban view "
-             "when there are no records in that stage "
-             "to display.")
-    company_id = fields.Many2one(
-        'res.company',
-        string="Company",
-        default=lambda self: self.env['res.company']._company_default_get(
-            'helpdesk.ticket')
-    )    
+    user_id = fields.Many2one('res.users',string='User', store=True)
+    is_quality = fields.Boolean(string='Quality')
+    
