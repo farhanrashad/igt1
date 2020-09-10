a4f428b1e18cc516eb069f863123407a35aadb93
dynexcel-ddc1 <ddc1@dynexcel.co>
2020-09-10 17:42:21 +0500

    vehicle request & employee visa

--- de_hr_employee_visa/__manifest__.py
+++ de_hr_employee_visa/__manifest__.py
@@ -1,22 +1,25 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "de_hr_employee_visa",
+    'name': "Employee Visa",
 
-    'summary': "Employee passport and visa Information",
+    'summary': """
+        Employee passport and visa Information""",
 
-    'description': "Employee visa",
+    'description': """
+        Employee visa
+    """,
 
-    'author': "My Company",
-    'website': "http://www.Dynexcel.com",
+    'author': "Dynexcel",
+    'website': "http://www.Dynexcel.co",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Dynexcel',
+    'category': 'HR',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','hr'],
+    'depends': ['base' , 'hr'],
 
     # always loaded
     'data': [
--- de_hr_employee_visa/models/models.py
+++ de_hr_employee_visa/models/models.py
@@ -5,27 +5,59 @@ from odoo import models, fields, api
 
 class HrEmployee(models.Model):
     _inherit = 'hr.employee'
+    _name = 'hr.employee'
 
     visa_lines = fields.One2many('visa.card.details', 'visa_id')
 
     passport_number = fields.Char(String="Passport Number")
     passport_issue_date = fields.Date(String="Passport Issue Date")
     passport_expiry_date = fields.Date(String="Passport Expiry Date")
-    passport_country = fields.Many2one('res.country',String="Passport Expiry Date")
-    other_nationality = fields.Many2many('res.country',string="Other Nationality")
-    # visa fields record
+    passport_country = fields.Many2one('res.country', String="Passport Expiry Date")
+    other_nationality = fields.Many2many('res.country', string="Other Nationality")
 
 
 class VisaCardDetails(models.Model):
     _name = 'visa.card.details'
     _description = 'Visa Card Details'
 
-    stats = fields.Selection([('to_request', 'To Request'), ('in_process', 'In Process'), ('approve', 'Approve'), ('reject', 'Reject'), ('expired', 'Expired')], string='state', default='to_request')
-    visa_id = fields.Many2one('hr.employee',String="Visa Id")
+    @api.onchange('employee_id', 'visa_Type')
+    def onchange_des(self):
+        if self.employee_id and self.visa_Type:
+            self.description = str(self.visa_Type.name) + str(self.employee_id.name)
+
+    state = fields.Selection(
+        [('to_request', 'To Request'), ('in_process', 'In Process'), ('approve', 'Approve'), ('reject', 'Reject'),
+         ('expired', 'Expired')], string='status', default='to_request')
+    visa_id = fields.Many2one('hr.employee', String="Visa Id")
+    employee_id = fields.Many2one('hr.employee', String="Employee")
     description = fields.Char(string="Description")
-    visa_Type = fields.Char(string="Visa Type")
-    Visa_issuance_Date = fields.Date(string="Issuance Date")
-    Visa_Expiration_Date = fields.Date(string="Expiration Date")
-    Visa_country = fields.Many2one('res.country',string="Country")
-    no_of_entries = fields.Char(string="No. Of Entries")
+    visa_Type = fields.Many2one('visa.card.type', String="Visa Type", readonly=True, required=1,
+                                states={'to_request': [('readonly', False)]})
+    Visa_issuance_Date = fields.Date(string="Issuance Date", required=1, readonly=True,
+                                     states={'to_request': [('readonly', False)]})
+    Visa_Expiration_Date = fields.Date(string="Expiration Date", required=1, readonly=True,
+                                       states={'to_request': [('readonly', False)]})
+    Visa_country = fields.Many2one('res.country', string="Country", required=1, readonly=True,
+                                   states={'to_request': [('readonly', False)]})
+    no_of_entries = fields.Selection([('single_entry_visa', 'Single Entry Visa'),
+                                      ('double_entry_visa', 'Double Entry Visa'),
+                                      ('multiple_entry_visa', 'Multiple Entry Visa')])
     status = fields.Char(string="Status")
+
+    def to_request(self):
+        self.state = 'in_process'
+
+    def approve_action(self):
+        self.state = 'approve'
+
+    def reject_action(self):
+        self.state = 'reject'
+
+    def expired_action(self):
+        self.state = 'expired'
+
+
+class VisaCardType(models.Model):
+    _name = 'visa.card.type'
+
+    name = fields.Char(String="Visa Type", required=1)
