83517ee0fec72c06bb562cb0adf211268cef71b4
dynexcel-ddc1 <ddc1@dynexcel.co>
2020-09-29 03:24:17 +0000

    update salary

--- de_employee_advance_salary/models/.ipynb_checkpoints/hr_payslips-checkpoint.py
+++ de_employee_advance_salary/models/.ipynb_checkpoints/hr_payslips-checkpoint.py
@@ -7,8 +7,8 @@ class HrPayslipsInherit(models.Model):
     _inherit = 'hr.payslip'
     
     def compute_sheet(self):
-        for other_input in self.input_line_ids:
-            other_input.unlink()
+#         for other_input in self.input_line_ids:
+#             other_input.unlink()
         other_inputs = self.env['hr.payslip.input.type'].search([('code','=', 'ADV')])
         paid_amount = self.env['hr.employee.advance.salary'].search([('employee_id','=', self.employee_id.id),('state','=', 'paid'),('request_date','>=', self.date_from),('confirm_date','<=', self.date_to)])
         data = []
